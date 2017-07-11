77f302fc2b01e21814f68379f2fc416d1eeb0062
anamikagit <anamika.arya1990@gmail.com>
2017-07-11 13:05:46 +0530

    successfully created database and inserted values too

--- app/src/main/java/com/example/anamika/storeandsend/MainActivity.java
+++ app/src/main/java/com/example/anamika/storeandsend/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.anamika.storeandsend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.stetho.Stetho;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Stetho.initializeWithDefaults(this);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        /**
+         * CRUD Operations
+         * */
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting ..");
+        db.addContact(new Contact("Ravi", "9100000000"));
+        db.addContact(new Contact("Srinivas", "9199999999"));
+        db.addContact(new Contact("Tommy", "9522222222"));
+        db.addContact(new Contact("Karthik", "9533333333"));
+        db.addContact(new Contact("Arthik", "9533303333"));
+
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();
+
+        for (Contact cn : contacts) {
+            String log = "Id: "+cn.get_id()+" ,Name: " + cn.get_name() + " ,Phone: " + cn.get_phone_number();
+            // Writing Contacts to log
+            Log.d("Name: ", log);
+        }
     }
 }
